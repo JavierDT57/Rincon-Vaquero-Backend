5c0dc6f92cd417ec9f7698a148f496e0799fa38a
JavierDT57 <javierdolorestolentino@gmail.com>
2025-10-19 20:47:07 -0600

    correccion para endpoint me

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -12,13 +12,11 @@ router.post('/login', userController.login);
 router.post('/logout', userController.logout);
 router.post('/recover-password', userController.recoverPassword);
 // Endpoints de administración de usuarios (protegidos con admin)
+router.get('/me', auth, userController.me);
 router.get('/',    auth, requireAdmin, usersAdminCtrl.listUsers);
 router.get('/:id', auth, requireAdmin, usersAdminCtrl.getUser);
 router.put('/:id', auth, requireAdmin, usersAdminCtrl.updateUser);
 router.delete('/:id', auth, requireAdmin, usersAdminCtrl.deleteUser);
 
-
-// Ruta protegida
-router.get('/me', auth, userController.me);
 
 module.exports = router;
