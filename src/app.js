245cb3fa5e6e1f645d31e4efbfafab57d8e6a67c
JavierDT57 <javierdolorestolentino@gmail.com>
2025-11-25 20:00:10 -0600

    Cors fix

--- src/app.js
+++ src/app.js
@@ -33,7 +33,7 @@ const allowedOrigins = [
 
 app.use(cors({
   origin: function (origin, callback) {
-    if (!origin) return callback(null, true); // para Postman y Node
+    if (!origin) return callback(null, true); 
     if (allowedOrigins.includes(origin)) {
       return callback(null, true);
     }
